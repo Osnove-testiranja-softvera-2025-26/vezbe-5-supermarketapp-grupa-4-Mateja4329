213bb0c5db9406993780d2e2b03f7198ae50f3d6
Mateja4329 <mateja.gl12@gmail.com>
2026-04-15 12:33:36 +0200

    Vezbe 5 - uvod u testiranje

--- OTS2019_Supermarket.Test/CartTest.cs
+++ OTS2019_Supermarket.Test/CartTest.cs
@@ -8,8 +8,23 @@ using System.Threading.Tasks;
 
 namespace OTS_Supermarket.Test
 {
+    [TestFixture]
     public class CartTest
     {
+        // ok
+        [Test]
+        public void AddOneToCart_ShouldAddItemToCart_Success()
+        {
+            // Arrange
+            Cart cart = new Cart();
+            OTS_Supermarket.Models.Monitor monitor = new OTS_Supermarket.Models.Monitor("Dell", 1000);
 
+            // Act
+            cart.AddOneToCart(monitor);
+
+            // Assert
+            Assert.That(cart.Size, Is.EqualTo(1));
+
+        }
     }
 }
